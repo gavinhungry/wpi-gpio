892697626cea49fb18f42b1c807803836a56bfa4
Gavin Lloyd <gavinhungry@gmail.com>
2015-06-20 20:42:25 -0700

    Add package.json, script stub

---
+++ wpi-gpio.js
@@ -0,0 +1,11 @@
+/**
+ * wpi-gpio - Wrapper around the WiringPi gpio command-line utility
+ * https://github.com/gavinhungry/wpi-gpio
+ */
+
+(function() {
+  'use strict';
+
+  var gpio = module.exports;
+
+})();
