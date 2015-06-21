80c4732f2dbb56670f4996de139cc0076c495496
Gavin Lloyd <gavinhungry@gmail.com>
2015-06-20 21:58:55 -0700

    Add methods

--- wpi-gpio.js
+++ wpi-gpio.js
@@ -6,6 +6,121 @@
 (function() {
   'use strict';
 
+  var exec = require('child_process').exec;
+
   var gpio = module.exports;
+  gpio.BCM_GPIO = false;
+
+  var MODES = ['in', 'out', 'pwm', 'up', 'down', 'tri'];
+
+  /**
+   * Exec a call to gpio
+   *
+   * @param {String} method
+   * @param {Array} [args]
+   * @param {Function} [callback] (err, stdout)
+   */
+  var gpioExec = function(method, args, callback) {
+    var flag = gpio.BCM_GPIO ? '-g' : '';
+    var cmd = ['gpio', flag, method, (args || []).join(' ')].join(' ');
+
+    exec(cmd, function(err, stdout, stderr) {
+      if (typeof callback !== 'function') {
+        return;
+      }
+
+      if (err) {
+        callback(stderr);
+        return;
+      }
+
+      callback(null, stdout);
+    });
+  };
+
+  var ensure = {
+    num: function(num) {
+      return parseInt(num, 10) || 0;
+    },
+
+    mode: function(mode) {
+      return MODES.indexOf(mode) >= 0 ? mode : MODES[0];
+    },
+
+    bin: function(val) {
+      return val ? 1 : 0;
+    },
+
+    pwm: function(pwm) {
+      pwm = ensure.num(pwm);
+      return pwm < 0 ? 0 : (pwm > 1023 ? 1023 : pwm);
+    }
+  };
+
+  /**
+   * Set a pin mode
+   *
+   * @param {Number} pin
+   * @param {String} mode
+   * @param {Function} [callback]
+   */
+  gpio.mode = function(pin, mode, callback) {
+    pin = ensure.num(pin);
+    mode = ensure.mode(mode);
+    return gpioExec('mode', [pin, mode], callback);
+  };
+
+  /**
+   * Read the value of a pin
+   *
+   * @param {Number} pin
+   * @param {Function} [callback]
+   */
+  gpio.read = function(pin, callback) {
+    pin = ensure.num(pin);
+    return gpioExec('read', [pin], function(err, val) {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      callback(null, ensure.bin(val));
+    });
+  };
+
+  /**
+   * Set the value of a pin
+   *
+   * @param {Number} pin
+   * @param {Boolean} val
+   * @param {Function} [callback]
+   */
+  gpio.write = function(pin, val, callback) {
+    pin = ensure.num(pin);
+    val = ensure.bin(val);
+    return gpioExec('write', [pin, val], callback);
+  };
+
+  /**
+   * Set the PWM value of a pin
+   *
+   * @param {Number} pin
+   * @param {Number} pwm
+   * @param {Function} [callback]
+   */
+  gpio.pwm = function(pin, pwm, callback) {
+    pin = ensure.num(pin);
+    pwm = ensure.pwm(pwm);
+    return gpioExec('pwm', [pin, pwm], callback);
+  };
+
+  /**
+   * Get pin table
+   *
+   * @param {Function} [callback]
+   */
+  gpio.readall = function(callback) {
+    return gpioExec('readall', null, callback);
+  };
 
 })();
