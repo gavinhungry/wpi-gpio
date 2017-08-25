de0ec47c5fa1fcf6e79cfa5698a121245b7b9b4b
Gavin Lloyd <gavinhungry@gmail.com>
2017-08-25 11:07:06 -0700

    Update tests

--- test/index.js
+++ test/index.js
@@ -21,9 +21,14 @@
     }
   });
 
-  tap.test('should set pin as input', function(t) {
+  tap.beforeEach(function(done) {
     cmds = [];
     gpio.BCM_GPIO = false;
+
+    done();
+  });
+
+  tap.test('should set pin as input', function(t) {
     return gpio.input(3).then(function() {
       t.same(cmds, ['gpio mode 3 in'], 'input method executes correct command');
     }).then(function() {
@@ -36,8 +41,6 @@
   });
 
   tap.test('should set pin as output', function(t) {
-    cmds = [];
-    gpio.BCM_GPIO = false;
     return gpio.output(4).then(function() {
       t.same(cmds, ['gpio mode 4 out'], 'output method executes correct command');
     }).then(function() {
@@ -50,8 +53,6 @@
   });
 
   tap.test('should set pin as output with initial value', function(t) {
-    cmds = [];
-    gpio.BCM_GPIO = false;
     return gpio.output(5, 1).then(function() {
       t.same(cmds, ['gpio write 5 1', 'gpio mode 5 out'], 'output method with initial value executes correct commands');
     }).then(function() {
@@ -63,10 +64,44 @@
     });
   });
 
+  tap.test('should set pin as input with pull-up resistor', function(t) {
+    return gpio.pullUp(4).then(function() {
+      t.same(cmds, ['gpio mode 4 up'], 'input method executes correct command');
+    }).then(function() {
+      cmds = [];
+      gpio.BCM_GPIO = true;
+      return gpio.pullUp(4).then(function() {
+        t.same(cmds, ['gpio -g mode 4 up'], 'input method with BCM_GPIO executes correct command');
+      });
+    });
+  });
+
+  tap.test('should set pin as input with pull-down resistor', function(t) {
+    return gpio.pullDown(4).then(function() {
+      t.same(cmds, ['gpio mode 4 down'], 'input method executes correct command');
+    }).then(function() {
+      cmds = [];
+      gpio.BCM_GPIO = true;
+      return gpio.pullDown(4).then(function() {
+        t.same(cmds, ['gpio -g mode 4 down'], 'input method with BCM_GPIO executes correct command');
+      });
+    });
+  });
+
+  tap.test('should set pin as input with tri-state', function(t) {
+    return gpio.triState(4).then(function() {
+      t.same(cmds, ['gpio mode 4 tri'], 'input method executes correct command');
+    }).then(function() {
+      cmds = [];
+      gpio.BCM_GPIO = true;
+      return gpio.triState(4).then(function() {
+        t.same(cmds, ['gpio -g mode 4 tri'], 'input method with BCM_GPIO executes correct command');
+      });
+    });
+  });
+
   tap.test('should read pin value', function(t) {
-    cmds = [];
     stdout = '1';
-    gpio.BCM_GPIO = false;
     return gpio.read(6).then(function(val) {
       t.same(cmds, ['gpio read 6'], 'read method executes correct command');
       t.equal(val, 1, 'read method returns numeric value');
@@ -81,8 +116,6 @@
 
   // this test is duplicated by testing gpio.output with an initial value
   tap.test('should write pin value', function(t) {
-    cmds = [];
-    gpio.BCM_GPIO = false;
     return gpio.write(7, 0).then(function() {
       t.same(cmds, ['gpio write 7 0'], 'write method executes correct command');
     }).then(function() {
@@ -95,8 +128,6 @@
   });
 
   tap.test('should write pin sequence', function(t) {
-    cmds = [];
-    gpio.BCM_GPIO = false;
     return gpio.sequence(8, [0, 1, 0]).then(function() {
       t.same(cmds, [
         'gpio mode 8 out', 'gpio write 8 0', 'gpio write 8 1', 'gpio write 8 0'
@@ -113,8 +144,6 @@
   });
 
   tap.test('should write pin tap sequence', function(t) {
-    cmds = [];
-    gpio.BCM_GPIO = false;
     return gpio.tap(9).then(function() {
       t.same(cmds, [
         'gpio mode 9 out', 'gpio write 9 1', 'gpio write 9 0', 'gpio write 9 1'
